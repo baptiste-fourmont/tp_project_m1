c6933c854b9930dad793e121c38bdc3921b34a66
Baptiste Fourmont <68250131+baptiste-fourmont@users.noreply.github.com>
2023-11-11 20:39:58 +0100

    [FIX] Create Products but does not seems to show it

--- src/backend/src/main/java/com/m1csc/db/backend/Controllers/ProductController.java
+++ src/backend/src/main/java/com/m1csc/db/backend/Controllers/ProductController.java
@@ -1,14 +1,19 @@
 package com.m1csc.db.backend.Controllers;
 
+import com.m1csc.db.backend.Entities.CategoryEntity;
 import com.m1csc.db.backend.Entities.EmployeeEntity;
 import com.m1csc.db.backend.Entities.ProductEntity;
+import com.m1csc.db.backend.Entities.SupplierEntity;
+import com.m1csc.db.backend.Services.CategoryService;
 import com.m1csc.db.backend.Services.ProductService;
+import com.m1csc.db.backend.Services.SupplierService;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-
 
 @Controller
 @RequestMapping("/products")
@@ -16,10 +21,18 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+    @Autowired
+    private CategoryService categoryService;
+    @Autowired
+    private SupplierService supplierService;
 
     @GetMapping("/new")
     public String showProductForm(Model model) {
-        model.addAttribute("product", new EmployeeEntity());
+        List<CategoryEntity> categories = categoryService.getCategories();
+        List<SupplierEntity> suppliers = supplierService.getSuppliers();
+        model.addAttribute("categories", categories);
+        model.addAttribute("suppliers", suppliers);
+        model.addAttribute("product", new ProductEntity());
         return "ProductForm";
     }
 
@@ -32,7 +45,9 @@ public class ProductController {
     @GetMapping("")
     public String showProducts(Model model) {
         model.addAttribute("products", productService.getProducts());
-        return "Products";
+        model.addAttribute("categories", categoryService.getCategories());
+        model.addAttribute("suppliers", supplierService.getSuppliers());
+        return "Products"; // This should match the name of your Thymeleaf template
     }
 
     @GetMapping("/edit/{id}")
@@ -58,7 +73,7 @@ public class ProductController {
     public String deleteProduct(@PathVariable Long id) {
         ProductEntity product = null;
 
-        try{
+        try {
             product = productService.getProductById(id).get();
         } catch (Exception e) {
             return "redirect:/products";
