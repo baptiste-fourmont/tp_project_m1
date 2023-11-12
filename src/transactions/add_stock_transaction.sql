b2413736289281e9acddcd7858954f621006966e
Baptiste <68250131+baptiste-fourmont@users.noreply.github.com>
2023-11-12 17:25:29 +0100

    Update add_stock_transaction.sql

--- src/transactions/add_stock_transaction.sql
+++ src/transactions/add_stock_transaction.sql
@@ -1,12 +1,7 @@
 -- Démarrer une transaction
-BEGIN;
+\c stock_management
 
--- Tenter d'insérer une nouvelle transaction
-BEGIN
+BEGIN;
     INSERT INTO stock_schema.transactions (transaction_date, employee_id, product_id, quantity_changed, transaction_type)
     VALUES (CURRENT_DATE, 1, 1, 10, 'BUY');
-EXCEPTION WHEN OTHERS THEN
-    ROLLBACK;
-    RAISE EXCEPTION 'Erreur lors de l''insertion de la transaction. Transaction annulée.';
-END;
 COMMIT;
