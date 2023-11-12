07253058153b74d1b424006469e702d3640cb6fc
Baptiste <68250131+baptiste-fourmont@users.noreply.github.com>
2023-11-12 17:26:05 +0100

    Update modifier_stock_transaction.sql

--- src/transactions/modifier_stock_transaction.sql
+++ src/transactions/modifier_stock_transaction.sql
@@ -1,14 +1,8 @@
-BEGIN;
+\c stock_management
 
-BEGIN
+
+BEGIN;
     UPDATE stock_schema.transactions
     SET quantity_changed = 15
     WHERE transaction_id = 1;
-EXCEPTION WHEN OTHERS THEN
-    -- En cas d'erreur, annuler la transaction
-    ROLLBACK;
-    RAISE EXCEPTION 'Erreur lors de la mise à jour de la transaction. Transaction annulée.';
-END;
-
--- Valider la transaction si aucune erreur n'est survenue
 COMMIT;
