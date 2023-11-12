d7418c0b30edf84dd66fd670a5d4773ee5feee4e
Baptiste <68250131+baptiste-fourmont@users.noreply.github.com>
2023-11-12 17:25:55 +0100

    Update del_stock_transaction.sql

--- src/transactions/del_stock_transaction.sql
+++ src/transactions/del_stock_transaction.sql
@@ -1,15 +1,7 @@
--- Démarrer une transaction
+\c stock_management
+
 BEGIN;
-
--- Tenter de supprimer une transaction
-BEGIN
     DELETE FROM stock_schema.transactions
-    WHERE transaction_id = 1;
-EXCEPTION WHEN OTHERS THEN
-    -- En cas d'erreur, annuler la transaction
+    WHERE transaction_id = 2;
     ROLLBACK;
-    RAISE EXCEPTION 'Erreur lors de la suppression de la transaction. Transaction annulée.';
-END;
-
--- Valider la transaction si aucune erreur n'est survenue
 COMMIT;
